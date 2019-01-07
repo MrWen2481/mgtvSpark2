7eb72e3229a95bd8e651b974d21cefc7893f22c9
bigdata <1234342443@qq.com>
2019-01-07 15:28:31 +0800

    修改移动90天活跃用户数多区域问题

--- src/main/java/com/starv/yd/User90Online.scala
+++ src/main/java/com/starv/yd/User90Online.scala
@@ -84,6 +84,85 @@ object User90Online {
          |                 AND t1.source_type   =               t2.source_type
          """.stripMargin
     conn.createStatement().execute(sql)
+
+    //更新移动sdk长沙区域用户数,长沙=总数-其他非长沙区域用户数
+    val result2 =  spark.sql(
+      s"""
+         |SELECT t2.total-t1.sumnum as stbnum,'14301' as regionid,'HNYD' as platform,'sdk' as source_type
+         |FROM
+         |(select count(DISTINCT uuid) as sumnum,'1' as id from owlx.mid_online_day where dt >= '$ninetyAgoDay'
+         |AND dt <='$dt' AND platform='HNYD' AND source_type='sdk' and regionid not in('14301')) as t1
+         | join
+         |(select count(distinct uuid) as total,'1' as id from owlx.mid_online_day
+         |WHERE dt >= '$ninetyAgoDay' AND dt <='$dt' AND platform='HNYD' AND source_type='sdk') as t2
+         |on t1.id=t2.id
+      """.stripMargin).collect()
+
+    val statement2 = conn.prepareStatement(
+      """
+        |UPDATE starv_stb_num SET stb_num  = ?
+        |WHERE create_date = ?
+        |AND area_code = ?
+        |AND platform = ?
+        |AND source_type = ?
+      """.stripMargin)
+    result2.foreach(x=>{
+      statement2.setLong(1,x.getLong(0))
+      statement2.setString(2,dt)
+      statement2.setString(3,x.getString(1))
+      statement2.setString(4,x.getString(2))
+      statement2.setString(5,StarvConst.getSourceTypeName(x.getString(2),x.getString(3)) )
+      statement2.addBatch()
+    })
+    statement2.executeBatch()
+
+    //合并移动数据源
+    sql =
+      s"""
+         |UPDATE starv_stb_num t1,
+         |                       (SELECT
+         |                          area_code,
+         |                          sum(stb_num   ) AS stb_num   ,
+         |                          platform
+         |                        FROM starv_stb_num
+         |                        WHERE create_date = '$dt'
+         |                        AND platform = 'HNYD'
+         |                        AND source_type NOT LIKE '%ALL'
+         |                        GROUP BY area_code,platform) t2
+         |                      SET
+         |                        t1.stb_num = t2.stb_num
+         |                      WHERE
+         |                               t1.create_date =     '$dt'
+         |                           AND t1.area_code   =     t2.area_code
+         |                           AND t1.platform    =     t2.platform
+         |                           AND t1.source_type =     'HNYD_ALL'
+         |
+         """.stripMargin
+    conn.createStatement().execute(sql)
+
+    //合并移动区域
+    sql =
+      s"""
+         | UPDATE starv_stb_num t1,
+         |                (SELECT sum(stb_num) AS stb_num ,
+         |                 source_type,
+         |                 platform
+         |                 FROM
+         |                 starv_stb_num
+         |                 WHERE
+         |                 create_date = '$dt'
+         |                 AND area_code <> '14300'
+         |                 AND platform      = 'HNYD'
+         |                 GROUP BY source_type) t2
+         |                 SET
+         |                 t1.stb_num           =  t2.stb_num
+         |                 WHERE t1.create_date =  '$dt'
+         |                 AND t1.area_code     =  '14300'
+         |                 AND t1.platform      =  t2.platform
+         |                 AND t1.source_type   =  t2.source_type
+         |
+         """.stripMargin
+    conn.createStatement().execute(sql)
     conn.commit()
     conn.close()
   }
